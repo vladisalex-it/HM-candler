0a6aa21544f35151149eb1f25df70c1d2919098d
Vladislav Zakharov <vl4disalex@gmail.com>
2025-10-10 23:59:24 +0300

    Create and adapt header, footer, and large welcome section of the page for various devices

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const menuButtonElement = document.querySelector('.menu__btn')
+const menuListElement = document.querySelector('.header__menu-list')
+
+menuButtonElement.addEventListener('click', function() {
+    menuListElement.classList.toggle('menu--open')
+})
