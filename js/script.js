e802abf16f1e6360601ffded37af2cb09e30b01f
vladisalex-it <vl4disalex@gmail.com>
2025-11-15 23:56:29 +0300

    add new accordeon-block with adaptation and javascript logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+const accordeonTitleButtonElements = document.querySelectorAll('.accordeon-title')
+const accordeonContentElements = document.querySelector('.accordeon-content')
 
+accordeonTitleButtonElements.forEach(accordeonBtn => {
+    accordeonBtn.addEventListener('click', () => {
+        accordeonBtn.nextElementSibling.classList.toggle('hidden')
+    })
+});
