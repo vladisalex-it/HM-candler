53a5026b8850d401f668c0ceb167e70781764be5
vladisalex-it <vl4disalex@gmail.com>
2025-11-16 23:53:39 +0300

    feat: Rewrite accordion for better code; add 2 new sections

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+const accordeon = document.querySelector('.accordeon')
 const accordeonTitleButtonElements = document.querySelectorAll('.accordeon-title')
-const accordeonContentElements = document.querySelector('.accordeon-content')
 
-accordeonTitleButtonElements.forEach(accordeonBtn => {
-    accordeonBtn.addEventListener('click', () => {
-        accordeonBtn.nextElementSibling.classList.toggle('hidden')
+accordeonTitleButtonElements.forEach.call(accordeonTitleButtonElements, function(accordeonTitle) {
+    accordeonTitle.addEventListener('click', () => {
+        const currentText = accordeonTitle.parentElement.querySelector('.accordeon-content')
+        
+        accordeonTitle.classList.toggle('accordeon-title--active')
+        currentText.classList.toggle('accordeon-content--visible')
+        
+        if (currentText.classList.contains('accordeon-content--visible')) {
+            currentText.style.maxHeight = currentText.scrollHeight + 'px'
+            accordeonTitle.classList.add('accordeon-title--no-padding-bottom')
+        } else {
+            currentText.style.maxHeight = null
+            accordeonTitle.classList.remove('accordeon-title--no-padding-bottom')
+        }
     })
 });
